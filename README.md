26742cd7563ed7e755e5b9d3e313a6b296a076ba
eugenioclrc <eugenioclrc@gmail.com>
2024-10-31 15:45:53 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,14 +1,14 @@
-Built to help out reddit user u/feroun_ (https://www.reddit.com/r/ethereum/comments/6grkoc/how_to_generate_a_big_password_list_for_a_brute/dissqi7/), who wrote down his password wrong and suspected it was only a few characters off.
+# Keystore wallet cracker
 
-REQURES LIST OF PASSWORDS TO BE GENERATED SEPARATELY!
-(see brutedistance repository...)
+Naively implemented in Python, ultra slow, but works, based on [Phildo/ethbrute](https://github.com/Phildo/ethbrute)
 
-If you want my help using this to crack your ETH wallet, lemme know! Contact me at phildo211 via gmail.
-If you want to use this yourself- go right ahead!
+## Usage
 
-if you end up using this and successfully unlocking your wallet, please consider donating!
+```shell
+python2 brute.py -w wallet.json -p rockyou-60.txt
+```
 
-My ETH Wallet:
+## Requirements
 
-![0xef2bebd1059446f3fc83203c24364e65bd42d103](https://raw.githubusercontent.com/Phildo/ethbrute/master/eth.png)
-
+- Python 2.7
+- `scrypt_kdf` dependency: `pip install scrypt_kdf`
--- brute.py
+++ brute.py
@@ -1,25 +1,50 @@
+## This script is used modified from https://github.com/Phildo/ethbrute/blob/master/brute.py
+
 from multikdf.scrypt import scrypt_kdf
-import hashlib
+#import hashlib
 import sha3
 import json
+import math
 
-with open('../wallet.json') as wallet_file:
+import argparse, sys
+
+parser=argparse.ArgumentParser()
+
+parser.add_argument("-w", help="Wallet file, example: -w wallet.json")
+parser.add_argument("-p", help="Password file list, example: -p rockyou-60.txt")
+
+args=parser.parse_args()
+
+if args.w == None or args.p == None:
+    print(parser.format_help())
+    sys.exit()
+
+wallet = args.w
+passwords = args.p
+
+with open(wallet) as wallet_file:
   wallet = json.load(wallet_file)
 
 dklen = 32
 salt = bytearray.fromhex(wallet["crypto"]["kdfparams"]["salt"])
-r = 8
-p = 1
-n = 18 #262144 #NOTE- put Log BASE2 n here. so, if n = 262144 in the .json wallet file, put 18 here
+r = wallet["crypto"]["kdfparams"]["r"]
+p = wallet["crypto"]["kdfparams"]["p"]
+n = wallet["crypto"]["kdfparams"]["n"]
+#262144 #NOTE- put Log BASE2 n here. so, if n = 262144 in the .json wallet file, put 18 here
+if(n == 262144):
+    n_log2 = 18
+else:
+    n_log2 = int(math.log2(n))
+    print(f"Converting n from {n} to log2(n) = {n_log2}")
 ciphertext = bytearray.fromhex(wallet["crypto"]["ciphertext"])
 mac = bytearray.fromhex(wallet["crypto"]["mac"])
 
-file = open("../password.txt", "r")
+file = open(passwords, "r")
 for line in file:
     password = line.strip('\n')
     print ("trying "+password)
 
-    derived_key = scrypt_kdf(password, salt, r, p, n, dklen)[16:32]
+    derived_key = scrypt_kdf(password, salt, r, p, n_log2, dklen)[16:32]
     concat = derived_key + ciphertext
 
     k = sha3.keccak_256()
@@ -29,4 +54,3 @@ for line in file:
     if hashconcat == mac:
         print (password + " WORKED!")
         exit()
-
